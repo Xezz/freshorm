8b03f79756a0de4d7e7e97cce2596c3cabbaf02e
Xezz <koch@xezz.org>
2013-07-16 20:44:56 +0200

    Initial commit

---
+++ src/main/java/com/example/Library.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import android.content.Context;
+
+public class Library {
+
+    public static String getHelloMessage(Context context) {
+        return context.getString(R.string.hello);
+    }
+
+}
